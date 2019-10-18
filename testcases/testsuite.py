39ed7c1f6e688038b6c8013c26a2cfc27b8e207e
ptlzc <37324971+ptlzc@users.noreply.github.com>
2019-10-18 15:42:53 +0800

    Update testsuite.py

--- testcases/testsuite.py
+++ testcases/testsuite.py
@@ -21,6 +21,8 @@ from testcases.mdm_portal.mutations.test_execute_appInstall import ExecuteAppIns
 from testcases.mdm_portal.mutations.test_enroll_and_unenroll import EnrollAndUnenroll
 from testcases.configuration.queries.test_configuration_list import ConfigurationList
 from testcases.configuration.queries.test_configuration_detail import ConfigurationDetail
+from testcases.configuration.queries.test_get_applied_panel import GetAppliedPanel
+from testcases.configuration.queries.test_get_deploy_config import GetDeployConfig
 from testcases.configuration.mutations.test_create_configuration import CreateConfiguration
 from testcases.configuration.mutations.test_update_configuration import UpdateConfiguration
 from testcases.configuration.mutations.test_delete_configuration import DeleteConfiguration
@@ -52,12 +54,38 @@ suite.addTest(UpdatePanelTags('test_update_panel_tags'))
 suite.addTest(DeletePanelTags('test_delete_panel_tags'))
 suite.addTest(ExecuteAppInstall('test_executeAppInstall'))
 suite.addTest(EnrollAndUnenroll('test_enroll_panel'))
+
+# ConfigurationList 共9个用例 林泳
 suite.addTest(ConfigurationList('test_configurationList'))
+suite.addTest(ConfigurationList('test_pagenumber_2'))
+suite.addTest(ConfigurationList('test_inputpageSize_is0'))
+suite.addTest(ConfigurationList('test_inputpageSize_is_plural_1'))
+suite.addTest(ConfigurationList('test_inputpageSize_is_plural_2'))
+suite.addTest(ConfigurationList('test_filters_name_is_empty'))
+suite.addTest(ConfigurationList('test_filters_name_is_true'))
+suite.addTest(ConfigurationList('test_configurationId_is_empty'))
+suite.addTest(ConfigurationList('test_configurationId_is_error'))
+
+# CreateConfiguration 共2个用例 林泳
 suite.addTest(CreateConfiguration('test_create_configuration'))
+suite.addTest(CreateConfiguration('test_configurationId_name_is_existed'))
+
 suite.addTest(ConfigurationDetail('test_configuration_detail'))
+
+# GetAppliedPanel 共3个用例 林泳
+suite.addTest(GetAppliedPanel('test_get_applied_panel'))
+suite.addTest(GetAppliedPanel('test_not_applied_of_Panel'))
+suite.addTest(GetAppliedPanel('test_applied_of_Panel'))
+
+suite.addTest(GetDeployConfig('test_get_deploy_config'))
+
 suite.addTest(UpdateConfiguration('test_update_configuration'))
 suite.addTest(DeleteConfiguration('test_delete_configuration'))
+
+# reapply 重复测试 丁正辉
 suite.addTest(Reapply('test_reapply'))
+
+# 场景测试共7个用例 陈烨婷
 suite.addTest(Combined1('test1'))
 suite.addTest(Combined1('test2'))
 suite.addTest(Combined1('test3'))
